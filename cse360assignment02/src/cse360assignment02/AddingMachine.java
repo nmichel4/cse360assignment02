6452f00e26da2f62d930e2809db192b06543e426
noah <noah@noahs-mbp.lan>
2020-10-07 18:10:57 -0700

    Initial Check

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () { 
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
